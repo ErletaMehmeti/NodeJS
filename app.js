e06ff31e2670a6db24495e961d56315a642c762d
ErletaMehmeti <erletamehmeti96@gmail.com>
2025-01-23 20:03:28 +0300

    Dynamic content

--- app.js
+++ app.js
@@ -1,49 +1,28 @@
-// Express vazhdim
-// var express=require("express")
-// var app = express()
+const express=require('express');
+const bodyParser=require('body-parser');
 
-// app.get('/users/:id', function(req,res){
-//     var users=[
-//         {id:1,name: 'fisteku'},
-//         {id:2,name: 'filan'}
-//     ]
-//     const user= users.find(item => {
-//         return item.id == req.params.id;
-//     })
-//     res.send('pershendetje'+ user.name)
-// });
-// app.listen(8011)
+// Konfigurimi i express per me perdor EJS
+const app=express();
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded ({extended: true}))
 
-//detyra 1,2
-const express = require('express');
-const app = express();
-const port = 3000;
+// Renderimi i index.ejs
+app.get('/',(req,res)=>{
+    res.ender('index');
+})
 
-const librat = [
-    { id: 1, titulli: 'The Power of Now', autori: ' Eckhart Tolle' },
-    { id: 2, titulli: 'Atomic Habits', autori: 'James Clear' },
-    { id: 3, titulli: 'Thinking, Fast and Slow', autori: ' Daniel Kahneman' },
-];
-
-const produktet = [
-    { id: 1, emri: 'Patika', cmimi: 100 },
-    { id: 2, emri: 'Çante', cmimi: 200 },
-    { id: 3, emri: 'Skincare', cmimi: 300 },
-];
-
-app.get('/librat', (req, res) => {
-    res.json(librat);
-});
-
-app.get('/produkte', (req, res) => {
-    res.json(produktet);
-});
-
-app.listen(port, () => {
-    console.log(`Serveri po punon ne http://localhost:${port}`);
-});
-
-
-
-
+// Path per perpunimin e formes (Sign Up Form)
+app.post('/', (req,res)=> {
+    const username = req.body.username;
+    const password = req.body.password;
+ 
+    console.log("Username:" + username);
+    console.log("Password:" + password);
+ 
+    res.render('Welcome', {emri: username, pw:password})
+ })
+ 
+ app.listen(3000, ()=>{
+     console.log('Serveri eshte startuar ne porten 3000')
+ })
 
